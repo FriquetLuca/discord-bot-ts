ce287b02e947e8bb4e36255ecc657640d3854d00
FriquetLuca <friquet.luca@gmail.com>
2023-12-07 19:11:21 +0100

    Update sunday schedule so it assign roles after leaderboard validation

--- src/startups/sundaySchedule.ts
+++ src/startups/sundaySchedule.ts
@@ -31,6 +31,39 @@ export function sundaySchedule(client: Client) {
         isCurrentSeason: false,
       })
 
+      const n1 = extermination_list[0] as { user_id: string; total_kills: number; } | undefined
+      const n2 = extermination_list[1] as { user_id: string; total_kills: number; } | undefined
+      const n3 = extermination_list[2] as { user_id: string; total_kills: number; } | undefined
+
+      const all_guilds = await prisma.mHWITopRoles.findMany()
+      const guildManager = client.guilds.cache
+
+      all_guilds.forEach(guild => {
+        const currentGuild = guildManager.find(g => g.id === guild.guild_id)
+        if(currentGuild) {
+          currentGuild.members.cache.forEach(member => {
+            if(n1?.user_id === member.user.id) {
+              if(!member.roles.cache.find(role => role.id === guild.first_id)) {
+                member.roles.add(guild.first_id)
+              }
+            } else if(n2?.user_id === member.user.id) {
+              if(!member.roles.cache.find(role => role.id === guild.second_id)) {
+                member.roles.add(guild.second_id)
+              }
+            } else if(n3?.user_id === member.user.id) {
+              if(!member.roles.cache.find(role => role.id === guild.third_id)) {
+                member.roles.add(guild.third_id)
+              }
+            } else {
+              member.roles.remove(guild.first_id);
+              member.roles.remove(guild.second_id);
+              member.roles.remove(guild.third_id);
+            }
+            
+          })
+        }
+      })
+
       sendOnNamedChannels(client, {
         channelName: "mh-kills",
         message: generateTopSeasonalHuntersText(extermination_list)

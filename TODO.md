9b9f9f9a3ed70d7db698073bf4b3b07f4e5e2c3c
FriquetLuca <friquet.luca@gmail.com>
2023-12-12 00:22:44 +0100

    add quantity to articles + add/remove article command

---
+++ TODO.md
@@ -0,0 +1,16 @@
+Shopping articles:
+- EditShopCartArticle
+- MyShopCartArticles
+  - Show total price
+  - Show unpaid sum
+  - Show refunded sum
+  - Show all articles in pagination
+    - Show the article name
+    - Show the article hash
+    - Show the article updated date if updated
+    - Show the article price
+    - Show the article quantity
+
+Also to design/do
+- A way to view the articles paid for us
+- A way to set the articles paid for us as refunded
--- src/mods/commands/shopping/articles/AddShopCartArticle.ts
+++ src/mods/commands/shopping/articles/AddShopCartArticle.ts
@@ -4,7 +4,7 @@ import { bold, italic } from "discord.js"
 
 export const AddShopCartArticle = chatCommandBuilder()
   .setName("add-shop-cart-article")
-  .setDescription("Ajouter un article à votre caddie.")
+  .setDescription("Ajouter un article à votre caddie contextuel.")
   .addStringOption(option =>
     option.setName("label")
     .setRequired(true)
@@ -14,6 +14,11 @@ export const AddShopCartArticle = chatCommandBuilder()
     option.setName("price")
     .setRequired(true)
     .setDescription("Le prix de l'article")
+  )
+  .addNumberOption(option =>
+    option.setName("quantity")
+    .setRequired(true)
+    .setDescription("La quantité de fois que l'article a été acheté (un nombre strictement positif)")
   )
   .addUserOption(option =>
     option.setName("recipient")
@@ -25,6 +30,7 @@ export const AddShopCartArticle = chatCommandBuilder()
     const currentLabel = interaction.options.get("label")?.value as string
     const currentPrice = parseMoney(interaction.options.get("price")?.value as string)
     const currentRecipient = interaction.options.get("recipient")?.value as string|undefined
+    const currentQuantity = Math.max(1, Math.floor(interaction.options.get("quantity")?.value as number))
 
     await interaction.deferReply()
 
@@ -60,11 +66,34 @@ export const AddShopCartArticle = chatCommandBuilder()
       return
     }
 
+    const cart = await prisma.shoppingMember.findFirst({
+      where: {
+        user_id: interaction.user.id,
+        cart_id: shopContext.hash,
+      },
+      select: {
+        cart: {
+          select: {
+            id: true,
+            label: true,
+          }
+        },
+      }
+    })
+
+    if(cart === null) {
+      await interaction.followUp({
+        content: `Erreur : Le caddie contextuel n'existe pas.`
+      })
+      return
+    }
+
     const newArticle = await prisma.shoppingArticle.create({
       data: {
         cart_id: shopContext.hash,
         label: currentLabel,
         price: currentPrice,
+        quantity: currentQuantity,
         recipient: currentRecipient,
       }
     })
